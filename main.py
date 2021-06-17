a806d0c27a643ac504ac5b287809909232bca9d4
AmirCM <amirbehzadfar.h@gmail.com>
2021-06-17 22:19:49 +0430

    Result visualized

--- main.py
+++ main.py
@@ -1,115 +1,58 @@
 import torch.optim as optim
 import scipy.io as sio
+import time
 import torch
+import h5py
 import matplotlib.pyplot as plt
-from torchvision import transforms, datasets
 import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 import cv2 as cv
 import numpy as np
 
-IS_Rebuild = 0
-
-
-class CleanVsBlurred:
-    CLEAN = 'PET'
-    BLURRED = 'blurred'
-    LABELS = {BLURRED: 0, CLEAN: 1}
-    training_data = []
-
-    def make_training_data(self):
-        mat_content = sio.loadmat('data.mat')
-        print(mat_content.keys())
-
-        for lbl in self.LABELS:
-            for i in tqdm(range(256)):
-                img = mat_content[lbl][:, :, i]
-                self.training_data.append([img, np.eye(2)[self.LABELS[lbl]]])
-                cv.imshow(lbl, img)
-                cv.waitKey(1)
-        np.random.shuffle(self.training_data)
-        np.save("training_data.npy", self.training_data)
-
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self._to_linear = 128 * 28 * 28
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(5, 5))
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(5, 5))
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(5, 5))
 
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.conv3 = nn.Conv2d(64, 128, 5)
+        x = torch.randn(256, 256).view(-1, 1, 256, 256)
+        self._to_linear = None
+        self.convs(x)
 
-        self.fc1 = nn.Linear(self._to_linear, 512)  # flattening.
-        self.fc2 = nn.Linear(512, 2)  # 512 in, 2 out bc we're doing 2 classes (clean vs blurred).
+        self.fc1 = nn.Linear(self._to_linear, 512)
+        self.fc2 = nn.Linear(512, 2)
 
-    def forward(self, x):
+    def convs(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
-        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)  # bc this is our output layer. No activation here.
+        x = self.fc2(x)
         return F.softmax(x, dim=1)
 
 
-def train(net, train_X, train_y):
-    optimizer = optim.Adam(net.parameters(), lr=0.0008)
-    loss_function = nn.MSELoss()
+class CleanVsBlurred:
+    CLEAN = 'cleans'
+    BLURRED = 'blurred'
+    LABELS = {BLURRED: 0, CLEAN: 1}
+    training_data = []
     BATCH_SIZE = 16
-    EPOCHS = 10
-
-    for epoch in range(EPOCHS):
-        loss = 0
-        for i in tqdm(range(0, len(train_X),
-                            BATCH_SIZE)):  # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
-            # print(f"{i}:{i+BATCH_SIZE}")
-            batch_X = train_X[i:i + BATCH_SIZE].view(-1, 1, 256, 256)
-            batch_y = train_y[i:i + BATCH_SIZE]
-
-            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-
-            net.zero_grad()
-
-            outputs = net(batch_X)
-            loss = loss_function(outputs, batch_y)
-            loss.backward()
-            optimizer.step()  # Does the update
-
-        print(f"Epoch: {epoch}. Loss: {loss}")
-
-
-def test(net, test_X, test_y):
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for i in tqdm(range(len(test_X))):
-            print(test_y[i])
-            real_class = torch.argmax(test_y[i])
-
-            net_out = net(test_X[i].view(-1, 1, 256, 256).to(device))[0]  # returns a list,
-            predicted_class = torch.argmax(net_out).to(device)
-
-            print(net_out)
-
-            """img = test_X[i] * 255
-            cv.imshow(f'{i}: {predicted_class}, {real_class}', img.view(256, 256).cpu().detach().numpy())
-            cv.waitKey(0)
-            cv.destroyWindow(f'{i}: {predicted_class}, {real_class}')"""
-
-            if predicted_class == real_class:
-                correct += 1
-            total += 1
-    print("Accuracy: ", round(correct / total, 3))
-
-
-if __name__ == "__main__":
-    clean_vs_blurred = CleanVsBlurred()
-    if IS_Rebuild:
-        clean_vs_blurred.make_training_data()
-    else:
-        clean_vs_blurred.training_data = np.load("training_data.npy", allow_pickle=True)
+    EPOCHS = 5
+    train_X = None
+    train_y = None
+    test_X = None
+    test_y = None
 
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
@@ -119,22 +62,90 @@ if __name__ == "__main__":
         print('CPU')
 
     net = Net().to(device)
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
 
-    X = torch.Tensor([i[0] for i in clean_vs_blurred.training_data]).view(-1, 256, 256)
-    X = X / 255.0
-    y = torch.Tensor([i[1] for i in clean_vs_blurred.training_data])
+    def __init__(self, IS_Rebuild=0):
+        if IS_Rebuild == 1:
+            self.make_training_data()
+        elif IS_Rebuild == 2:
+            self.training_data = np.load("Ltraining_data.npy", allow_pickle=True)
+        else:
+            self.training_data = np.load("training_data.npy", allow_pickle=True)
 
-    VAL_PCT = 0.1  # lets reserve 10% of our data for validation
-    val_size = int(len(X) * VAL_PCT)
-    print(val_size)
+        X = torch.Tensor([i[0] for i in self.training_data]).view(-1, 256, 256)
+        X = X / 255.0
+        y = torch.Tensor([i[1] for i in self.training_data])
 
-    train_X = X[:-val_size]
-    train_y = y[:-val_size]
-    train(net, train_X, train_y)
+        VAL_PCT = 0.1
+        val_size = int(len(X) * VAL_PCT)
+        print(val_size)
 
-    test_X = X[-val_size:]
-    test_y = y[-val_size:]
+        self.train_X = X[:-val_size]
+        self.train_y = y[:-val_size]
 
-    test_X = test_X.to(device)
-    test_y = test_y.to(device)
-    test(net, test_X, test_y)
+        self.test_X = X[-val_size:]
+        self.test_y = y[-val_size:]
+
+    def make_training_data(self):
+        # mat_content = sio.loadmat('dataset.mat')
+        mat_content = h5py.File('data.mat', 'r')
+        print('Successfully Loaded:', mat_content.keys())
+
+        for lbl in self.LABELS:
+            print(lbl, mat_content[lbl].shape)
+            ones_array = np.ones((256, 1))
+            for i in tqdm(range(256)):
+                img = mat_content[lbl][:, :, i]
+
+                if sum(img.dot(ones_array)) > 0:
+                    self.training_data.append([img, np.eye(2)[self.LABELS[lbl]]])
+
+        print(len(self.training_data))
+        np.random.shuffle(self.training_data)
+        np.save("training_data.npy", self.training_data)
+
+    def fwd_pass(self, X, y, train=False):
+        if train:
+            self.net.zero_grad()
+        outputs = self.net(X)
+
+        matches = [torch.argmax(i) == torch.argmax(j) for i, j in zip(outputs, y)]
+
+        acc = matches.count(True) / len(matches)
+        loss = self.loss_function(outputs, y)
+
+        if train:
+            loss.backward()
+            self.optimizer.step()
+
+        return int(acc * 100), int(loss * 100)
+
+    def test(self):
+        X, y = self.test_X, self.test_y
+        val_acc, val_loss = self.fwd_pass(X.view(-1, 1, 256, 256).to(self.device), y.to(self.device))
+        return val_acc, val_loss
+
+    def train(self):
+        MODEL_NAME = f"model-{int(time.time())}"
+        print(MODEL_NAME)
+        with open("model.log", "a") as f:
+            for epoch in range(self.EPOCHS):
+                for i in tqdm(range(0, len(self.train_X), self.BATCH_SIZE)):
+                    batch_X = self.train_X[i:i + self.BATCH_SIZE].view(-1, 1, 256, 256)
+                    batch_y = self.train_y[i:i + self.BATCH_SIZE]
+
+                    batch_X, batch_y = batch_X.to(self.device), batch_y.to(self.device)
+
+                    acc, loss = self.fwd_pass(batch_X, batch_y, train=True)
+
+                    if i % 32 == 0:
+                        val_acc, val_loss = self.test()
+                        f.write(
+                            f"{MODEL_NAME},{round(time.time(), 3)},{round(float(acc), 2)},{round(float(loss), 4)},"
+                            f"{round(float(val_acc), 2)},{round(float(val_loss), 4)},{epoch}\n")
+
+
+if __name__ == "__main__":
+    clean_vs_blurred = CleanVsBlurred()
+    clean_vs_blurred.train()
